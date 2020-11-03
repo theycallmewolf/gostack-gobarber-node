a4516a73fed1b076d1b43b864b2859e974feb0b1
bruno-wolf <bruno.m.lobato@gmail.com>
2020-11-03 19:47:11 +0000

    retrieved request data on appointments route/post (tested)

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,10 +1,24 @@
 import { Router } from 'express';
+import { v4 as uuid } from 'uuid';
 
 const appointmentsRouter = Router();
 
+const appointments = [];
+
 // POST http://localhost:3333/appointments
-appointmentsRouter.post('/', (request, response) =>{
-  return response.json({message: 'hello wolf 🐺🤟'});
+appointmentsRouter.post('/', (request, response) => {
+
+  const { provider, date } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  }
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
