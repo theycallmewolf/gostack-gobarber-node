7df6fcc47433b0aef5cd5beca0de266782f8702f
bruno-wolf <bruno.m.lobato@gmail.com>
2020-11-03 23:21:42 +0000

    added Appointment model

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,14 +1,8 @@
 import { Router } from 'express';
-import { v4 as uuid } from 'uuid';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/appointment';
 
 const appointmentsRouter = Router();
-
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
 
 const appointments: Appointment[] = [];
 
@@ -26,11 +20,7 @@ appointmentsRouter.post('/', (request, response) => {
     return response.status(400).json({ message: 'this appointment is already booked' })
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  }
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
