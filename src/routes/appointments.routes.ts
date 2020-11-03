80daf82e1ec14ac09d4f30bfb1d00502ceebbc44
bruno-wolf <bruno.m.lobato@gmail.com>
2020-11-03 19:56:42 +0000

    formatted received date on request with date-fns

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { v4 as uuid } from 'uuid';
+import { startOfHour, parseISO } from 'date-fns';
 
 const appointmentsRouter = Router();
 
@@ -10,10 +11,12 @@ appointmentsRouter.post('/', (request, response) => {
 
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   }
 
   appointments.push(appointment);
