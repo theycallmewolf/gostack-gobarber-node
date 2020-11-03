c947bdc483e2db31ad9069da9d96aeda3f0b4e42
bruno-wolf <bruno.m.lobato@gmail.com>
2020-11-03 23:02:22 +0000

    added validation for 2 appointments in the same date

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,30 @@
 import { Router } from 'express';
 import { v4 as uuid } from 'uuid';
-import { startOfHour, parseISO } from 'date-fns';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 // POST http://localhost:3333/appointments
 appointmentsRouter.post('/', (request, response) => {
 
   const { provider, date } = request.body;
-
   const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response.status(400).json({ message: 'this appointment is already booked' })
+  }
 
   const appointment = {
     id: uuid(),
