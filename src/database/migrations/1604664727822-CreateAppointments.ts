8b92e1d4b860f3166a428e6932cee483d7f2b5e0
bruno-wolf <bruno.m.lobato@gmail.com>
2020-11-06 17:24:15 +0000

    conected to db using typeorm and added CreateAppointments migration

---
+++ src/database/migrations/1604664727822-CreateAppointments.ts
@@ -0,0 +1,38 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table
+} from "typeorm";
+
+export class CreateAppointments1604664727822 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [{
+          name: 'id',
+          type: 'varchar', // normally an integer but in this case is an varchar, since we are using uuid that returns a string
+          isPrimary: true,
+          generationStrategy: 'uuid'
+        },
+        {
+          name: 'provider',
+          type: 'varchar',
+          isNullable: false,
+        },
+        {
+          name: 'date',
+          type: 'timestamp with time zone', // 'timestamp with time zone' apenas existe no postgres guardando o horário e fuso-horário, se usarmos outra db temos de colocar apenas 'timestamp'
+          isNullable: false,
+        }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> { 
+    await queryRunner.dropTable('appointments')
+  }
+
+}
