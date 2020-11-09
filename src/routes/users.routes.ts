9291685ad183788f57d0e7109eaf454f6812e7a0
bruno-wolf <bruno.m.lobato@gmail.com>
2020-11-09 22:17:07 +0000

    added password hash when create a new user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -11,7 +11,16 @@ usersRouter.post('/', async (request, response) => {
     const createUser = new CreateUserService();
 
     const user = await createUser.execute({ name, email, password });
-    return response.json(user);
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json(userWithoutPassword);
   } catch (err) {
     return response.status(400).json({ error: err.message })
   }
