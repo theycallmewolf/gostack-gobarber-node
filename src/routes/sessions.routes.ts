e2501dc498e346133f9df8b4021b317b9804b925
bruno-wolf <bruno.m.lobato@gmail.com>
2020-11-09 23:12:55 +0000

    added validation to user authenticate

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import AuthenticateUserService from '../services/AuthenticateUserService';
+const sessionsRouter = Router();
+
+// POST
+sessionsRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticateUser = new AuthenticateUserService();
+
+    const { user } = await authenticateUser.execute({
+      email,
+      password
+    })
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at
+    }
+
+    return response.json({ userWithoutPassword });
+  } catch (err) {
+    return response.status(400).json({ error: err.message })
+  }
+});
+
+export default sessionsRouter;
---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,35 @@
+import User from '../models/User';
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<{ user: User }> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({
+      where: { email }
+    })
+
+    if (!user) {
+      throw new Error('wrong email/password combination');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('wrong email/password combination');
+    }
+
+    // user is now authenticated
+
+    return { user };
+  };
+}
+
+export default AuthenticateUserService;
