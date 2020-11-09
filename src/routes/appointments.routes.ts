a65e81a1fd3168c0fe845b9b0d065e50201150ec
bruno-wolf <bruno.m.lobato@gmail.com>
2020-11-09 21:46:42 +0000

    fix: replace provider to provider_id on CreateAppointmentService and appointments.routes

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -17,13 +17,13 @@ appointmentsRouter.get('/', async (request, response) => {
 // POST http://localhost:3333/appointments
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
     const parsedDate = parseISO(date);
 
     const createAppointment = new CreateAppointmentService();
     const appointment = await createAppointment.execute({
       date: parsedDate,
-      provider: provider
+      provider_id
     });
 
     return response.json(appointment);
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -4,13 +4,13 @@ import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
 interface RequestDTO {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
 
-  public async execute({ provider, date }: RequestDTO): Promise<Appointment> {
+  public async execute({ provider_id, date }: RequestDTO): Promise<Appointment> {
 
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
@@ -25,7 +25,7 @@ class CreateAppointmentService {
     }
 
     const appointment = appointmentsRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     });
 
