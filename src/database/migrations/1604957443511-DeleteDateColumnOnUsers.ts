a14907f0283f385743579bae53943ae00de74174
bruno-wolf <bruno.m.lobato@gmail.com>
2020-11-09 21:35:05 +0000

    fix: added DeleteDateColumnOnUsers

---
+++ src/database/migrations/1604957443511-DeleteDateColumnOnUsers.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export default class DeleteDateColumnOnUsers1604957443511 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn('users', 'date');
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn('users', new TableColumn({
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+        }));
+    }
+
+}
