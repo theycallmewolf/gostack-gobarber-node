1338061af217b7e24915e99dbebc5137b1f64caf
bruno-wolf <bruno.m.lobato@gmail.com>
2020-11-12 23:04:47 +0000

    added service to update user avatar

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,9 +2,14 @@ import multer from "multer";
 import path from "path";
 import crypto from "crypto";
 
+const tempFolder = path.resolve(__dirname, "..", "..", "tmp");
+
 export default {
+
+  directory: tempFolder,
+
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, "..", "..", "tmp"),
+    destination: tempFolder,
     filename: (request, file, callback) => {
       const fileHash = crypto.randomBytes(10).toString("hex");
       const fileName = `${fileHash}-${file.originalname}`;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -36,8 +37,25 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
-    return response.json({ ok: true });
+    try {
+      const UpdateUserAvatar = new UpdateUserAvatarService();
+      const user = await UpdateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename
+      })
+
+      const userWithoutPassword = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+        created_at: user.created_at,
+        updated_at: user.updated_at
+      }
+      return response.json(userWithoutPassword);
+    } catch (err) {
+
+    }
   },
 );
 
