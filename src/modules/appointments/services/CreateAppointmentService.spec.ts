24dc391edc55d54526384ed5b9c0099f9fc45dce
bruno-wolf <bruno.m.lobato@gmail.com>
2020-12-14 23:16:54 +0000

    adds test to avoid creating two appointments at the same date

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,5 +1,6 @@
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
+import AppError from '@shared/errors/AppError';
 
 describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
@@ -15,7 +16,22 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('123123123');
   })
 
-  // it('should not be able to create two appointments at the same date', () => {
-  //   expect(1 + 2).toBe(3);
-  // })
+  it('should not be able to create two appointments at the same date', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11); //11:00 10 May 2020
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123123'
+    });
+
+    expect(createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123123'
+    })).rejects.toBeInstanceOf(AppError);
+  })
 })
