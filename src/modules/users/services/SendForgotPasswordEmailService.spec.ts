8106d593f552305cc3f7a3eade7fd5d0782bad2a
bruno-wolf <bruno.m.lobato@gmail.com>
2020-12-29 11:45:55 +0000

    adds shouldnt be able to recover a non-existing user password test

--- src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -3,6 +3,7 @@
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
 import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+import AppError from '@shared/errors/AppError';
 
 describe('SendForgotPasswordEmail', () => {
   it('should be able to recover the password using his email', async () => {
@@ -27,6 +28,23 @@ describe('SendForgotPasswordEmail', () => {
     })
 
     expect(sendMail).toHaveBeenCalled();
+  })
+
+  it('shouldn\'t be able to recover a non-existing user password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    jest.spyOn(fakeMailProvider, 'sendMail');
+
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider
+    );
+
+    await expect(sendForgotPasswordEmail.execute({
+      email: 'fake@theycallmewolf.com',
+    })).rejects.toBeInstanceOf(AppError);
+
   })
 
 })
