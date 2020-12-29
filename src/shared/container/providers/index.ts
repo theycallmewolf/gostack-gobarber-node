e3dd984bcb05a5652b5f24cd1d9631f79ef9096b
bruno-wolf <bruno.m.lobato@gmail.com>
2020-12-29 20:32:05 +0000

    adds mail template provider using handlebars lib

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -6,11 +6,20 @@ import DiskStorageProvider from './StorareProvider/implementations/DiskStoragePr
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+
 container.registerSingleton<IStorageProvider>(
-  'StorageProvider', DiskStorageProvider,
+  'StorageProvider',
+  DiskStorageProvider,
 )
 
 container.registerInstance<IMailProvider>(
-  'MailProvider', new EtherealMailProvider(),
+  'MailProvider',
+  new EtherealMailProvider(),
 )
 
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider,
+)
