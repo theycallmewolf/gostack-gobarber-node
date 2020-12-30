37d0dc9a78ff35f94bc8cdf6687276e7f1dadd44
bruno-wolf <bruno.m.lobato@gmail.com>
2020-12-30 20:11:34 +0000

    adds findAllProviders method to UsersRepository

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -2,9 +2,20 @@ import { v4 as uuid } from 'uuid';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 import User from "../../infra/typeorm/entities/User";
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 
 class FakeUsersRepository implements IUsersRepository {
   private users: User[] = [];
+
+  public async findAllProviders({ except_user_id }: IFindAllProvidersDTO): Promise<User[]> {
+    let { users } = this;
+
+    if (except_user_id) {
+      users = this.users.filter(user => user.id !== except_user_id);
+    }
+
+    return users;
+  }
 
   public async findById(id: string): Promise<User | undefined> {
     const findUser = this.users.find(user => user.id === id);
