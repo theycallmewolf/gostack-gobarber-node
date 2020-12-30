f1eb372b952afa3cc2f66c03391f5c83090f56ff
bruno-wolf <bruno.m.lobato@gmail.com>
2020-12-30 23:09:26 +0000

    lists available days on ListProviderMonthAvailabilityService

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentRepository';
 
@@ -27,9 +28,26 @@ class ListProviderMonthAvailabilityService {
       year,
     });
 
-    console.log(appointments);
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
 
-    return [{ day: 1, available: false }];
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (value, index) => index + 1,
+    );
+    // [1, 2, 3, 4, ..., 31]
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      })
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
   }
 }
 
