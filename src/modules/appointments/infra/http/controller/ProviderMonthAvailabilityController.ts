ba29fed0df3407f125a7236aa91f309b1d7613d4
bruno-wolf <bruno.m.lobato@gmail.com>
2021-01-02 20:26:57 +0000

    adds providers availability controllers and routes

---
+++ src/modules/appointments/infra/http/controller/ProviderMonthAvailabilityController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import ListProviderMonthAvailabilityService from "@modules/appointments/services/ListProviderMonthAvailabilityService";
+
+export default class ProviderMonthAvailabilityController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+    const { month, year } = request.body;
+
+    const lstProviderMonthAvailability = container.resolve(ListProviderMonthAvailabilityService);
+    const availability = await lstProviderMonthAvailability.execute({
+      provider_id,
+      month,
+      year
+    });
+
+    return response.json(availability);
+  }
+}
--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import ensureAuthenticated from "@modules/users/infra/http/middlewares/ensureAuthenticated";
 import ProvidersController from '../controller/ProvidersController';
+import ProviderMonthAvailabilityController from '../controller/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '../controller/ProviderDayAvailabilityController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get("/", providersController.create);
+providersRouter.get("/:provider_id/monthly-availability", providerMonthAvailabilityController.create);
+providersRouter.get("/:provider_id/daily-availability", providerDayAvailabilityController.create);
 
 export default providersRouter;
