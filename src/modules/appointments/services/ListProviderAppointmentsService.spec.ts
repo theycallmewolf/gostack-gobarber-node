0b7f055cc8e7b738fa846c3688a00c467d665201
bruno-wolf <bruno.m.lobato@gmail.com>
2021-01-02 21:31:45 +0000

    adds ListProviderAppointmentsService (with tests)

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,41 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository
+    );
+  });
+
+  it('should be able to list the appointments from a specific day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider_id',
+      user_id: 'user_id',
+      date: new Date(2021, 0, 3, 8, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider_id',
+      user_id: 'user_id',
+      date: new Date(2021, 0, 3, 12, 0, 0),
+    });
+
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id: 'provider_id',
+      day: 3,
+      year: 2021,
+      month: 1,
+    });
+
+    expect(appointments).toEqual([
+      appointment1,
+      appointment2
+    ]);
+  });
+})
---
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,36 @@
+import { inject, injectable } from 'tsyringe';
+
+import Appointment from '../infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '../repositories/IAppointmentRepository';
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+@injectable()
+class ListProviderAppointmentsService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository
+  ) { };
+
+  public async execute({
+    provider_id,
+    day,
+    month,
+    year
+  }: IRequest): Promise<Appointment[]> {
+    const appointments = this.appointmentsRepository.findAllInDayFromProvider({
+      provider_id,
+      day,
+      month,
+      year
+    })
+    return appointments;
+  }
+}
+
+export default ListProviderAppointmentsService;
