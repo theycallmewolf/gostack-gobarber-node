c48e921a806a179f24dcd5eccca815c7a388162c
bruno-wolf <bruno.m.lobato@gmail.com>
2021-01-02 16:52:55 +0000

    adds findAllInDayFromProvider method to Appointments repository

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -2,6 +2,7 @@ import { getRepository, Repository, Raw } from "typeorm";
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentRepository'
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import IFindAllInMonthFromProviderDTO from "@modules/appointments/dtos/IFindAllInMonthFromProviderDTO";
+import IFindAllInDayFromProviderDTO from "@modules/appointments/dtos/IFindAllInDayFromProviderDTO";
 
 import Appointment from "../entities/Appointment";
 
@@ -38,6 +39,30 @@ class AppointmentsRepository implements IAppointmentsRepository {
 
     return appointments;
   }
+
+
+  public async findAllInDayFromProvider({
+    provider_id,
+    day,
+    month,
+    year
+  }: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+    const parsedDay = String(day).padStart(2, '0');
+    const parsedMonth = String(month).padStart(2, '0');
+
+    const appointments = await this.ormRepository.find({
+      where: {
+        provider_id,
+        date: Raw(
+          dateFieldName =>
+            `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
+        ),
+      },
+    });
+
+    return appointments;
+  }
+
 
   public async create({ provider_id, date }: ICreateAppointmentDTO): Promise<Appointment> {
     const appointment = this.ormRepository.create({ provider_id, date });
