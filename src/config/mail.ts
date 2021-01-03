6729f39f8735422fd1f2e9cd56eb0a36acd1ec32
bruno-wolf <bruno.m.lobato@gmail.com>
2021-01-03 22:35:59 +0000

    configs AWS SES on SESMailProvider

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -1,7 +1,22 @@
 interface IMailConfig {
   driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string,
+      name: string,
+    }
+  }
 }
 
 export default {
   driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'bruno@theycallmewolf.com',
+      name: 'Bruno'
+    }
+  }
+
 } as IMailConfig;
