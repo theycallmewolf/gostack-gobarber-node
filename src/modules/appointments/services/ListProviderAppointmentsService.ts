8ed5bc188c66747607a8a67c1a2710715362529b
bruno-wolf <bruno.m.lobato@gmail.com>
2021-01-04 11:46:00 +0000

    configs regis cache provider

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentRepository';
+import ICacheConfig from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   provider_id: string;
@@ -14,7 +15,10 @@ interface IRequest {
 class ListProviderAppointmentsService {
   constructor(
     @inject('AppointmentsRepository')
-    private appointmentsRepository: IAppointmentsRepository
+    private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheConfig,
   ) { };
 
   public async execute({
@@ -23,12 +27,19 @@ class ListProviderAppointmentsService {
     month,
     year
   }: IRequest): Promise<Appointment[]> {
+
+    const cacheData = await this.cacheProvider.recover('test');
+    console.log(cacheData);
+
     const appointments = this.appointmentsRepository.findAllInDayFromProvider({
       provider_id,
       day,
       month,
       year
     })
+
+    // await this.cacheProvider.save('test', 'ok');
+
     return appointments;
   }
 }
