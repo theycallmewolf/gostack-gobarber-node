1953f7306fcbbd1a97ea91d1e9238159ea3d4fda
bruno-wolf <bruno.m.lobato@gmail.com>
2021-01-16 21:17:11 +0000

    fix: use class-transform on providers controller

--- src/modules/appointments/infra/http/controller/ProvidersController.ts
+++ src/modules/appointments/infra/http/controller/ProvidersController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 import ListProvidersService from "@modules/appointments/services/ListProvidersService";
 
 export default class ProvidersController {
@@ -9,6 +10,6 @@ export default class ProvidersController {
     const listProviders = container.resolve(ListProvidersService);
     const providers = await listProviders.execute({ user_id });
 
-    return response.json(providers);
+    return response.json(classToClass(providers));
   }
 }
